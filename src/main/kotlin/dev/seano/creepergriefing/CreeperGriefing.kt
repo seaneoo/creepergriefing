ce748c2caf31b07be4338bd2abfd805fa4faec08
Sean O'Connor <15949898+seaneoo@users.noreply.github.com>
2025-03-25 17:09:32 -0600

    Add creeperGriefing GameRule

--- src/main/kotlin/dev/seano/creepergriefing/CreeperGriefing.kt
+++ src/main/kotlin/dev/seano/creepergriefing/CreeperGriefing.kt
@@ -1,10 +1,21 @@
 package dev.seano.creepergriefing
 
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleFactory
+import net.fabricmc.fabric.api.gamerule.v1.GameRuleRegistry
+import net.minecraft.world.GameRules
 
 class CreeperGriefing : ModInitializer {
 
-    override fun onInitialize() {
+    companion object {
 
+        val gameRuleCreeperGriefing: GameRules.Key<GameRules.BooleanRule> =
+            GameRuleRegistry.register(
+                "creeperGriefing",
+                GameRules.Category.MOBS,
+                GameRuleFactory.createBooleanRule(true)
+            )
     }
+
+    override fun onInitialize() {}
 }
